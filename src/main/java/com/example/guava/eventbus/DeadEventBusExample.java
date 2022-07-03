6239a891659e4b672a9909c588d8b0720e5134dd
夏先鹏 <951256916>
2022-07-03 13:17:52 +0800

    unregister

--- src/main/java/com/example/guava/eventbus/DeadEventBusExample.java
+++ src/main/java/com/example/guava/eventbus/DeadEventBusExample.java
@@ -10,13 +10,21 @@ import com.google.common.eventbus.EventBus;
 public class DeadEventBusExample {
 
     public static void main(String[] args) {
+        final DeadEventListener deadEventListener = new DeadEventListener();
         final EventBus eventBus = new EventBus("DeadEventBus") {
             @Override
             public String toString() {
                 return "DEAD-EVENT-BUS";
             }
         };
-        eventBus.register(new DeadEventListener());
+        eventBus.register(deadEventListener);
         eventBus.post("Hello");
+
+        eventBus.unregister(deadEventListener);
+        eventBus.post("Hello");
+        eventBus.post("Hello");
+        eventBus.post("Hello");
+        eventBus.post("Hello");
+
     }
 }
